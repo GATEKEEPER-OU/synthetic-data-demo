fd16212ebf3251f6e582d9a66786375c01268280
AudreyEkuban <13083743+AudreyEkuban@users.noreply.github.com>
2022-09-06 11:41:43 +0100

    Implemented main demo

--- synthetic_data_demo/use_evaluator.py
+++ synthetic_data_demo/use_evaluator.py
@@ -2,6 +2,9 @@ from synthetic_data.evaluator.evaluator import SyntheticDataEvaluator
 
 dataset = "/a/dataset/path" # TODO get from the stdin
 
+codings_file = 'statics/codings.csv'
+evaluation_file = 'statics/evaluation.csv'
+
 tool = SyntheticDataEvaluator(
   # TODO params
   # - configure statics HERE
--- synthetic_data_demo/use_generator.py
+++ synthetic_data_demo/use_generator.py
@@ -1,18 +1,30 @@
 from synthetic_data.generator.generator import SyntheticDataGenerator
+import os
+from datetime import datetime
 
 n_patients = 10 # TODO get from the stdin
 n_days = 100 # TODO get from the stdin
 
+event_model = 'models/event_model.h5'
+timing_model = 'models/timing_model.h5'
+
+events_vocab = 'statics/events_vocab.json'
+timings_vocab = 'statics/timings_vocab.json'
+
+codings_file = 'statics/codings.csv'
+
+timestamp = datetime.now()
+output_dir = 'out/generated/%d' % timestamp
+os.makedirs(output_dir)
+
 tool = SyntheticDataGenerator(
-  # TODO params
-  # - configure statics HERE
-  # - configure models HERE
+  event_model, timing_model, events_vocab, timings_vocab, codings_file
 )
 
 print("Test one arg")
-tool.generate(n_patients)
+tool.generate(output_dir, n_patients)
 print()
 
-print("Test two args")
-tool.generate(n_patients, n_days)
-print()
+#print("Test two args")
+#tool.generate(n_patients, n_days)
+#print()
