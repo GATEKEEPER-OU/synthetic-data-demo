d392b13c60f524beedc1222fcb0066bdee8ed0be
Audrey Ekuban <81741182+AudreyE2@users.noreply.github.com>
2022-09-12 11:03:26 +0100

    Get Arguments from command line

--- synthetic_data_demo/use_generator.py
+++ synthetic_data_demo/use_generator.py
@@ -5,35 +5,48 @@ fpath = os.path.join(os.path.dirname(__file__), "..", "..", "synthetic-data")
 fpath = os.path.abspath(fpath)
 sys.path.append(fpath)
 
-from synthetic_data.generator.generator import SyntheticDataGenerator
+import argparse
 
-from datetime import datetime
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s <n_patients> <n_days>",
+        description="Generate data for <n_days> for <n_patients> and send to be evaluated"
+    )
+    parser.add_argument(
+        "-v", "--version", action="version",
+        version = f"{parser.prog} version 2.0-beta.1"
+    )
+    parser.add_argument('n_patients', nargs='?', type=int)
+    parser.add_argument('n_days', nargs='?', type=int)
+    return parser
 
-n_patients = 2 # TODO get from the stdin
-n_days = 2 # TODO get from the stdin
+def run(n_patients, n_days):
+    from synthetic_data.generator.generator import SyntheticDataGenerator
+    from datetime import datetime
 
-timestamp = datetime.now()
+    timestamp = datetime.now()
 
-# This is the directory that holds the files that are to be evaluated
-# Probably need another to hold the "real" files
-output_dir = os.path.join('out', 'generated', timestamp.strftime('%Y%m%d%H%M%S'))
-os.makedirs(output_dir)
+    # This is the directory that holds the files that are to be evaluated
+    output_dir = os.path.join('out', 'generated', timestamp.strftime('%Y%m%d%H%M%S'))
+    os.makedirs(output_dir)
 
-tool = SyntheticDataGenerator(n_patients, n_days=n_days)
+    tool = SyntheticDataGenerator(n_patients, n_days)
 
-print("Test one arg")
-tool.generate(output_dir)
-print()
+    print("Generating Data")
+    tool.generate(output_dir)
+    print()
 
+    # Simulation of file transfer.
+    #
+    print('Transfering files to shared....')
+    transfer_dir = os.path.join('transfer', 'generated')
+    tool.transfer(output_dir, transfer_dir)
+    print('End of files transfer')
 
-# Simulation of file transfer.
-#
-print('Transfering files to shared....')
-# output_dir = "C:\\Users\\abe29\\SAM-GK-SYSTEM\\code2\\synthetic-data-demo\\out\\generated\\20220908164424"
-transfer_dir = os.path.join('transfer', 'generated')
-tool.transfer(output_dir, transfer_dir)
-print('End of files transfer')
+def main():
+    parser = init_argparse()
+    args = parser.parse_args()
+    run(args.n_patients, args.n_days)
 
-#print("Test two args")
-#tool.generate(n_patients, n_days)
-#print()
+if __name__ == "__main__":
+    main()
