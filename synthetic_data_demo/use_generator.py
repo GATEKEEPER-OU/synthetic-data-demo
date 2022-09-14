97bfa7b0e9ad14dda30a6c2e875e23c9c1edd081
Audrey Ekuban <81741182+AudreyE2@users.noreply.github.com>
2022-09-14 08:00:33 +0100

    Updated tor reflect deployment process

--- synthetic_data_demo/use_generator.py
+++ synthetic_data_demo/use_generator.py
@@ -5,24 +5,11 @@ fpath = os.path.join(os.path.dirname(__file__), "..", "..", "synthetic-data")
 fpath = os.path.abspath(fpath)
 sys.path.append(fpath)
 
-import argparse
 
-def init_argparse() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(
-        usage="%(prog)s <n_patients> <n_days>",
-        description="Generate data for <n_days> for <n_patients> and send to be evaluated"
-    )
-    parser.add_argument(
-        "-v", "--version", action="version",
-        version = f"{parser.prog} version 2.0-beta.1"
-    )
-    parser.add_argument('n_patients', nargs='?', type=int)
-    parser.add_argument('n_days', nargs='?', type=int)
-    return parser
-
-def run(n_patients, n_days):
+def main(n_patients, n_days):
     from synthetic_data.generator.generator import SyntheticDataGenerator
     from datetime import datetime
+    import os
 
     timestamp = datetime.now()
 
@@ -36,17 +23,7 @@ def run(n_patients, n_days):
     tool.generate(output_dir)
     print()
 
-    # Simulation of file transfer.
-    #
-    print('Transfering files to shared....')
-    transfer_dir = os.path.join('transfer', 'generated')
-    tool.transfer(output_dir, transfer_dir)
-    print('End of files transfer')
-
-def main():
-    parser = init_argparse()
-    args = parser.parse_args()
-    run(args.n_patients, args.n_days)
-
 if __name__ == "__main__":
-    main()
+    # n_patients, n_days
+    # The UX is out of scope
+    main(5, 5)
