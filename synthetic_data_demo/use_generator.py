6bf220e8ab7c499c021f6a2ceeb2c9c03881c18d
Audrey Ekuban <81741182+AudreyE2@users.noreply.github.com>
2022-09-15 20:15:05 +0100

    Update use_generator.py

--- synthetic_data_demo/use_generator.py
+++ synthetic_data_demo/use_generator.py
@@ -5,27 +5,24 @@ fpath = os.path.join(os.path.dirname(__file__), "..", "..", "synthetic-data")
 fpath = os.path.abspath(fpath)
 sys.path.append(fpath)
 
+from datetime import datetime
+from synthetic_data.generator.generator import SyntheticDataGenerator
 
-def main(n_patients, n_days):
-    from synthetic_data.generator.generator import SyntheticDataGenerator
-    from datetime import datetime
-    import os
-    
-    model_dir =  os.path.join('models', '1')
-    
-    timestamp = datetime.now()
+def main():
+  models_dir =  os.path.join('models', '1')
 
-    # For this demo, this is the directory that holds the files that are to be evaluated
-    output_dir = os.path.join('out', 'generated', timestamp.strftime('%Y%m%d%H%M'))
-    os.makedirs(output_dir)
+  timestamp = datetime.now()
 
-    tool = SyntheticDataGenerator(n_patients, n_days)
+  # For this demo, this is the directory that holds the files that are to be evaluated
+  output_dir = os.path.join('out', 'generator', timestamp.strftime('%Y%m%d%H%M%S'))
+  if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
 
-    print("Generating Data")
-    tool.generate(model_dir, output_dir)
-    print()
+  syntdatag = SyntheticDataGenerator(models_dir)
+
+  print("Generating Data")
+  syntdatag.generate(output_dir, 1, 1)
+  print()
 
 if __name__ == "__main__":
-    # n_patients, n_days
-    # The UX is out of scope
-    main(1, 1)
+    main()
